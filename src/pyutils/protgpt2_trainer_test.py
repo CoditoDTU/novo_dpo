1999b137906d9747ee6cafa3efa7aade33e2d293
Codito <rogall@dtu.dk>
2025-04-10 08:08:57 +0000

    Adding test for puma

---
+++ src/pyutils/protgpt2_trainer_test.py
@@ -0,0 +1,71 @@
+import torch
+from torch.optim import Adam
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from ml_tools import *
+import matplotlib.pyplot as plt  # Import matplotlib for plotting
+from copy import deepcopy
+from datasets import load_dataset
+from trl import GRPOConfig, GRPOTrainer
+import os 
+import sys
+import json
+from datasets import Dataset
+from datasets import DatasetDict
+
+
+#PATHS
+TRAIN_DATA_PATH = "/home/developer/Projects/novo_dpo/data/processed/amp_train.json"
+TEST_DATA_PATH = "/home/developer/Projects/novo_dpo/data/processed/amp_test.json"
+OUTPUT_NAME = "protgpt2_test_rgd_0"
+
+#HYPERPARAMETERS:
+LEARNING_RATE = 1e-8
+LOGGING_STEPS = 1
+NUM_GENERATIONS = 8
+NUM_ITERATIONS = 10
+
+
+# ARGUMENTS
+config_dict = {
+    'output_dir': OUTPUT_NAME,
+    'logging_steps': LOGGING_STEPS,
+    'learning_rate': LEARNING_RATE,
+    'num_generations': NUM_GENERATIONS,
+    'remove_unused_columns': True,
+    'log_completions': True
+
+}
+
+
+def main():
+
+    # LOAD DATASETS
+
+    with open(TRAIN_DATA_PATH, "r") as f:
+        train_dataset = json.load(f)
+
+    with open(TEST_DATA_PATH, "r") as f:
+        test_dataset = json.load(f)
+
+    # COMPATIBLE WITH HF 
+    hf_train_dataset = Dataset.from_dict(train_dataset)
+    hf_test_dataset = Dataset.from_dict(test_dataset)
+
+    # GRPO config:
+
+    training_args = GRPOConfig(**config_dict)
+
+    #GRPO
+    trainer = GRPOTrainer(
+    model="NorseDrunkenSailor/ProtGPT2-with-pad",
+    reward_funcs=reward_hydrophobicity,
+    args=training_args,
+    train_dataset=hf_train_dataset,
+    )
+    
+    trainer.train()
+
+
+
+if __name__ == "__main__":
+    main()
