3045df884bbf21de748d29104d669089453500ba
Codito <rogall@dtu.dk>
2025-04-16 13:35:21 +0000

    Adding dpo run 4

--- dpo_test.py
+++ dpo_test.py
@@ -19,13 +19,13 @@ TOKENIZER = AutoTokenizer.from_pretrained("NorseDrunkenSailor/ProtGPT2-with-pad"
 
 # GRPO config args
 
-OUTPUT_NAME = 'DPO_protgpt2_3'
+OUTPUT_NAME = 'DPO_protgpt2_4'
 LOGGING_STEPS = 1
 BETA = 1
 LEARNING_RATE = 1e-3
 ADAM_BETAS = (0.9, 0.999) 
 ADAM_EPSILON = 1e-8
-N_TRAIN_EPOCHS = 4
+N_TRAIN_EPOCHS = 50
 ADAM_DECAY = 1e-2
 
 # %%
