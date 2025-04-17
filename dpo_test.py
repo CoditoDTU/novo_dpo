e412e7bf582ae4ce9acd59573993424fc6d04eb0
Codito <rogall@dtu.dk>
2025-04-17 11:37:02 +0000

    Adding dataset split + dpo of 1 epoch \OwO/

--- dpo_test.py
+++ dpo_test.py
@@ -11,21 +11,24 @@ from datasets import DatasetDict
 
 # GRPO Trainer args
 
-TRAIN_FILENAME = 'OXDA__activity_dpo.json'
+TRAIN_FILENAME = 'OXDA_activity_dpo_train.json'
+TEST_FILENAME = 'OXDA_activity_dpo_test.json'
+
 TRAIN_DATA_PATH = os.path.join(os.getcwd(), "src", "pyutils", TRAIN_FILENAME)
+TEST_DATA_PATH = os.path.join(os.getcwd(), "src", "pyutils", TEST_FILENAME)
 
 MODEL = AutoModelForCausalLM.from_pretrained("NorseDrunkenSailor/ProtGPT2-with-pad")
 TOKENIZER = AutoTokenizer.from_pretrained("NorseDrunkenSailor/ProtGPT2-with-pad")
 
 # GRPO config args
 
-OUTPUT_NAME = 'DPO_protgpt2_oxda_0'
+OUTPUT_NAME = 'DPO_protgpt2_oxda_1'
 LOGGING_STEPS = 1
 BETA = 0.1
 LEARNING_RATE = 1e-5
 ADAM_BETAS = (0.9, 0.999) 
 ADAM_EPSILON = 1e-8
-N_TRAIN_EPOCHS = 50
+N_TRAIN_EPOCHS = 3
 ADAM_DECAY = 0.1
 
 # %%
@@ -51,16 +54,21 @@ def main():
     with open(TRAIN_DATA_PATH, "r") as f:
         train_dataset = json.load(f)
 
-    hf_dataset = Dataset.from_dict(train_dataset)
+    with open(TEST_DATA_PATH, "r") as f:
+        test_dataset = json.load(f)
+
+    hf_train_dataset = Dataset.from_dict(train_dataset)
+    hf_test_dataset = Dataset.from_dict(test_dataset)
 
     training_args = DPOConfig(**config_dict)
 
     trainer = DPOTrainer(model = MODEL,
                          args = training_args,
-                         train_dataset = hf_dataset,
+                         train_dataset = hf_train_dataset,
+                         eval_dataset = hf_test_dataset,
                          processing_class = TOKENIZER)
     trainer.train()
-
+    trainer.evaluate()
 
 
 
